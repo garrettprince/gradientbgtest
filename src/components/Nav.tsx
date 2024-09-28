78249fc5814dc2edb6c6c984ec65864dfc444b21
your_github_username <garrettjamesprince@gmail.com>
2024-09-27 17:15:48 -0700

    sizing text update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ function Nav() {
       <div className="flex items-center justify-center space-x-2">
           <div className="flex items-center justify-center bg-white/30 backdrop-blur-sm py-2 px-4 rounded-full">
               <Search className="text-black w-4 h-4 mr-2" />
-              <p className="text-black font-semibold text-sm">SEARCH</p>
+              <p className="text-black font-semibold text-md">SEARCH</p>
           </div>
           <p className="text-black font-semibold bg-white/25 backdrop-blur-xl py-2 px-4 rounded-full text-sm">MENU</p>
       </div>
